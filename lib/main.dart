116327cbd89934be436c6cf472f99bb70c82502d
Solahuddeen kamae <6510110625@email.psu.ac.th>
2025-07-23 21:02:05 +0700

    add auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
-import 'pages/profile_page.dart'; 
+import 'pages/profile_page.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:campus_life_hub/pages/login/login.dart';
+import 'package:campus_life_hub/services/auth_service.dart';
+import 'pages/signup/signup.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login()
+    );
+  }
+}
+/* 
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -63,3 +99,4 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 }
+ */
