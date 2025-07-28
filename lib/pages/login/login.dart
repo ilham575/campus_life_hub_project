cf62a1440dee33d2318cd0eb815d23a6dc5bdfbb
Solahudeen <deensola02@gmail.com>
2025-07-29 01:11:16 +0700

    add splash sreen

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,6 +1,5 @@
 import 'package:campus_life_hub/pages/signup/signup.dart';
 import 'package:campus_life_hub/services/auth_service.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/services.dart';
